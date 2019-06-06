8e9afe7e92bbf33541bd5931962468d454702819
Veronica Sinitsa <veronicasinitsa@gmail.com>
2019-06-07 00:16:59 +0300

    Added +2

--- Lecture1/Task 1/myPackage/Main.java
+++ Lecture1/Task 1/myPackage/Main.java
@@ -2,6 +2,6 @@ package myPackage;
 
 public class Main {
     public static void main (String [] args){
-        System.out.println(1+2+10+100-9);
+        System.out.println(1+2+10+100-9+2);
     }
 }
